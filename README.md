aa1ed68076f5c4579d497b604a592f5ff3ad1e98
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2026-02-01 23:54:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# repo template
+
+This is a template repository. It make it easier to create a new project with the dependencies I need  
+The dependencies in this template are:
+
+- npm
+- webpack
+- eslint
+- prettier
+- jest
+- babel
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { 
+        ...globals.browser,
+        ...globals.jest,
+      },
+     },
+  },
+  {
+    rules: {
+      "no-self-assign": "error",
+      "no-unreachable": "error",
+      "use-isnan": "error",
+      "no-global-assign": "error",
+      "no-redeclare": "warn",
+      "no-useless-catch": "warn",
+      "no-useless-escape": "warn",
+      "no-irregular-whitespace": "warn",
+      "no-unused-vars": "warn",
+      "no-undef": "none",
+      "camelcase": "warn",
+      "curly": "warn",
+    },
+  },
+]);

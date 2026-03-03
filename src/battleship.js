28fbd3133ae707ced06dd85afa863ef4da3320ce
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2026-03-03 18:41:01 +0100

    feat: finish and test the ability for placing ships vertically in placeShip method

--- src/battleship.js
+++ src/battleship.js
@@ -23,11 +23,23 @@ function Ship(length) {
 }
 
 function Gameboard() {
+  // creates a 2d array with the length of 10 x 10
   const board = Array.from({ length: 10 }, () => Array(10).fill(0));
 
   function placeShip(ship, x, y, rotated = false) {
-    if (!rotated) {
+    ship.cords[0] = [x, y];
 
+    // if ship is rotated (vertically)
+    if (rotated) {
+      // if y axis plus the ship length is out of index of the array then move the ship so it fits
+      if (y + ship.length > board[x].length) {
+        y = board[x].length - ship.length;
+      } 
+
+      for (let i = 0; i < ship.length; i++) {
+        board[y + i][x] = 1;
+      }
+    } else { // if x axis plus the ship length is out of index of the array then move the ship so it fits
       if (x + ship.length > board[y].length) {
           x = board[y].length - ship.length;
       } 
@@ -35,9 +47,9 @@ function Gameboard() {
       for (let i = 0; i < ship.length; i++) {
         board[y][x + i] = 1;
       }
-    }
 
-    console.log(board);
+      console.log(board)
+    }
   }
 
   return {

68f3aafd894f4531fb2bdb7a6f2bded087ac1764
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2026-04-03 18:20:27 +0200

    feat: Finish the basic UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./styles.css";
 import { Ship, Gameboard, humanPlayer, computerPlayer } from "./battleship.js"
 
+const gameboards = document.querySelectorAll(".gameboard");
+
+function fillGameboard(gameboard) {
+     function createColumn() {
+        const column = document.createElement('div');
+        column.classList.toggle('column');
+        gameboard.appendChild(column);
+        function createBlock() {
+            const block = document.createElement('div');
+            block.classList.toggle('block');
+            column.appendChild(block);
+        }
+        for(let i = 0; i < 10; i++) {
+            createBlock();
+        }
+    }
+
+    for(let i = 0; i < 10; i++) {
+        createColumn();
+    }
+}
+
+for (const gameboard of gameboards) {
+    fillGameboard(gameboard);
+}

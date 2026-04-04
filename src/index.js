cf7bf9ed51d007bf511d057cd5c7f2a1452cebb7
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2026-04-04 20:25:59 +0200

    feat: Add buttons + startGame function + change fillGameboard function so player and computer cell will have different classes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import { Ship, Gameboard, humanPlayer, computerPlayer } from "./battleship.js"
 
 const gameboards = document.querySelectorAll(".gameboard");
 
-function fillGameboard(gameboard) {
+function fillGameboard(gameboardDiv) {
      function createColumn() {
         const column = document.createElement('div');
         column.classList.toggle('column');
-        gameboard.appendChild(column);
+        gameboardDiv.appendChild(column);
         function createBlock() {
             const block = document.createElement('div');
-            block.classList.toggle('block');
+            if (gameboardDiv.classList.contains("computer-gameboard")) {
+                block.classList.toggle("computer-block");
+            } else {
+                block.classList.toggle("player-block");
+            }
+            block.classList.toggle("block");
             column.appendChild(block);
         }
         for(let i = 0; i < 10; i++) {
@@ -23,6 +28,34 @@ function fillGameboard(gameboard) {
     }
 }
 
-for (const gameboard of gameboards) {
-    fillGameboard(gameboard);
+for (const gameboardDiv of gameboards) {
+    fillGameboard(gameboardDiv);
+}
+
+function getRandomPos() {
+    return Math.floor(Math.random() * 10);
+}
+
+function getRandomRotation() {
+    return Math.floor(Math.random()) > 0.5 ? "V" : "H";
+}
+
+function displayShips(gameboard) {
+
+}
+
+function startGame(humanPlayer, computerPlayer) {
+    const shipLengths = [1, 1, 2, 3, 4, 5];
+    for (const length in shipLengths) {
+        let playerX = getRandomPos();
+        let playerY = getRandomPos();
+        let playerRotation =  getRandomRotation();
+
+        let computerX = getRandomPos();
+        let computerY = getRandomPos();
+        let computerRotation =  getRandomRotation();
+
+        humanPlayer.gameboard.placeShip(Ship(length), playerX, playerY, playerRotation);
+        computerPlayer.gameboard.placeShip(Ship(length), computerX, computerY, computerRotation);
+    }
 }
